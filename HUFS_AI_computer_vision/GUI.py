17bdce4b87d00639bbb3adab50544bb09d867e3c
yousirong <diziyong1523@gmail.com>
2024-12-09 19:02:26 +0900

    20241209

--- HUFS_AI_computer_vision/GUI.py
+++ HUFS_AI_computer_vision/GUI.py
@@ -7,154 +7,205 @@ from PIL import Image
 from PIL import ImageTk
 from tkinter import messagebox
 import image_processing as ip
+from ImageConverter import ImageConverter
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from tkinter import messagebox
 
-#Graphic User Interface
+
+# =====================
+# Graphic User Interface
+# =====================
 class MyTkinter():
 
     def __init__(self):
-        self.window = tkinter.Tk()
+        """ 초기화 메서드 - instance 클래스 변수 초기화 """
+        self.window = tkinter.Tk()  # tkinter 윈도우 창 생성
 
-        self.img_source = 0 #실제, 소스, 타겟 이미지
-        self.img_target = 0
+        self.img_source = 0  # 원본 이미지 변수 초기화
+        self.img_target = 0  # 전처리 후 이미지 변수 초기화
 
-        self.img_sourceTK = 0 #렌더링 
-        self.img_targetTK = 0
+        self.img_sourceTK = 0  # tkinter용 원본 이미지 변수 초기화
+        self.img_targetTK = 0  # tkinter용 전처리 이미지 변수 초기화
 
-        self.ip = 0 #이미지 처리 객체
-        self.cam_update_id = 0 #캠을 관리하는 id
-        
+        self.ip = 0  # 이미지 처리 객체 (ImageProcessing 인스턴스) 변수 초기화
+        self.cam_update_id = 0  # 카메라 업데이트 ID 변수 초기화
+
+        self.selected_option = tkinter.IntVar(value=0)  # 라디오 버튼 값 변수 초기화
+        self.classNum = 0  # 클래스 개수 변수 초기화
+        self.imgGroupList = []  # 이미지 그룹 리스트 변수 초기화
+
     def initialize(self):
-        self.window.title("AI based Computer Vision Class")
-        self.window.geometry("500x500+200+200") #500x500 프레임웍의 크기 @ 200, 200 위치
-        self.window.resizable(True, True) #사이즈를 조정 가능하도록 허용
+        """ GUI 초기화 및 위젯 배치(버튼, 레이블, 프레임) 생성 """
+        self.window.title("AI based Computer Vision Class")  # 창 제목 설정
 
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+
+        window_width = 500
+        window_height = 500
+        x_pos = (screen_width - window_width) // 2
+        y_pos = (screen_height - window_height) // 2
+
+        self.window.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+        self.window.resizable(True, True)  # 창 크기 조절 가능
+
+        # 총 4개의 프레임 생성
         self.frameLeft = self.createFrame(self.window, "left")
         self.frameCenter = self.createFrame(self.window, "left")
         self.frameRight = self.createFrame(self.window, "left")
-        
+        self.frameRight2 = self.createFrame(self.window, "left")
+
+        # 버튼 생성
         self.b1 = self.createButtonGrid(self.frameLeft, "image open", 0, 0)
         self.b2 = self.createButtonGrid(self.frameLeft, "apply", 0, 1)
-
         self.b3 = self.createButtonGrid(self.frameLeft, "thresholding", 1, 0)
         self.b6 = self.createButtonGrid(self.frameLeft, "circle detection", 1, 1)
-
         self.b4 = self.createButtonGrid(self.frameLeft, "web cam mode", 2, 0)
-        self.b5 = self.createButtonGrid(self.frameLeft, "close cam", 2, 1)             
+        self.b5 = self.createButtonGrid(self.frameLeft, "close cam", 2, 1)
+        self.b7 = self.createButtonGrid(self.frameLeft, "capture", 3, 0)
+        self.b8 = self.createButtonGrid(self.frameLeft, "new class", 3, 1)
 
+        # 레이블 생성
         self.l1 = self.createLabel(self.frameCenter)
         self.l2 = self.createLabel(self.frameRight)
-        
+
+        # 버튼의 동작 연결
         self.b1.config(command=self.imgRead)
         self.b2.config(command=self.apply)
         self.b3.config(command=self.thresholding)
         self.b4.config(command=self.getCam)
         self.b5.config(command=self.stopCam)
         self.b6.config(command=self.circleDetection)
-        
-    def createFrame(self, window, pos, rel = "solid", border=2):
-        frame = tkinter.Frame(window, relief=rel, bd=border)
-        frame.pack(side=pos, fill="both", expand=True)
-        return frame
-    
-    def createLabel(self, window):
-        label = tkinter.Label(window)
-        label.pack(expand=True) #side="top"
-        return label
-        
-    def createButtonPlace(self, window, name, px=0, py=0, w=30, h=30):
-        button = tkinter.Button(window, text = name)
-        button.place(x=px, y=py, width=w, height=h)  
-        return button
-    
-    def createButtonGrid(self, window, name, r, c):
-        button = tkinter.Button(window, text = name)
-        button.grid(row = r, column = c)
-        return button
+        self.b7.config(command=self.capture)
+        self.b8.config(command=self.newClass)
 
-    def buttonEvent(self, button, event):
-        button.config(command=event)
+    def trainCNN(self):
+        """ CNN 학습 함수 컴퓨팅 파워 낮은 gpu used : titan xp vram 12G"""
+        try:
+            data_dir = "dataset"  # 데이터셋 디렉터리
+            img_size = 128  # 이미지 크기
+            batch_size = 32  # 배치 크기
+            epochs = 10  # 에포크 수
 
-    def getCam(self):
-        cap = cv.VideoCapture(0) #, cv.CAP_DSHOW
+            train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+            val_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
-        if not cap.isOpened():
-            messagebox.showinfo("Info", "fail to open camera")
-            return
+            # 데이터 로드
+            train_generator = train_datagen.flow_from_directory(
+                data_dir, target_size=(img_size, img_size), batch_size=batch_size, class_mode='categorical', subset='training'
+            )
+            val_generator = val_datagen.flow_from_directory(
+                data_dir, target_size=(img_size, img_size), batch_size=batch_size, class_mode='categorical', subset='validation'
+            )
+            # VGG 16 사용
+            # includ_top = False -> Fully Connected Layer(FC Layer)를 제거하고 특성 추출기로 사용
+            base_model = VGG16(weights='imagenet', include_top=False, input_shape=(img_size, img_size, 3))
+            for layer in base_model.layers:
+                layer.trainable = False  # VGG16의 레이어는 동결
 
-        def update_frame():
-            ret, img = cap.read()
-            if ret:
-                img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  
-                # OpenCV는 BGR, tkinter는 RGB를 사용하므로 변환 필요
+            # 커스텀 레이어 추가
+            x = Flatten()(base_model.output)
+            x = Dense(128, activation='relu')(x)
+            x = Dropout(0.5)(x)
+            predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+            # 옵티마이저는 Adam 사용 0.001
+            model = Model(inputs=base_model.input, outputs=predictions)
+            # 손실 함수로 categorical_crossentropy 사용
+            model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-                self.window.geometry(f"{int(img.shape[1]*2.5)}x{img.shape[0]}+200+200")
+            history = model.fit(train_generator, epochs=epochs, validation_data=val_generator)
+            model.save("cnn_model.h5")  # 모델 저장
 
-                self.img_target = img #self.img_source.copy()
-                self.img_source = Image.fromarray(img)
-                
-                self.img_sourceTK = ImageTk.PhotoImage(image=self.img_source)
+            self.plotTrainingResults(history)  # 학습 결과 시각화
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
 
-                self.l1.config(image=self.img_sourceTK)
+    def testCNN(self):
+        """ CNN 테스트 함수  """
+        try:
+            # 파일 대화 상자를 띄워 테스트에 사용할 이미지를 선택
+            model = load_model("/home/juneyonglee/Desktop/vision_class/cnn_model.h5")
+            file_path = filedialog.askopenfilename(title="Select a Test Image", filetypes=[("Image Files", "*.jpg;*.jpeg;*.png"), ("All Files", "*.*")])
+            if not file_path:
+                return
 
-            # 10ms 후에 다시 프레임 업데이트 (이렇게 하면 tkinter의 이벤트 루프가 멈추지 않음)
-            self.cam_update_id = self.window.after(10, update_frame)
+            img_size = 128
+            img = Image.open(file_path).resize((img_size, img_size))
+            img_array = np.array(img) / 255.0
+            img_array = np.expand_dims(img_array, axis=0)
+            # 모델의 예측 결과에서 가장 높은 확률의 클래스를 찾기
+            predictions = model.predict(img_array)
+            predicted_class = np.argmax(predictions, axis=1)[0]
+            confidence = np.max(predictions) * 100
+            class_labels = os.listdir("dataset")
 
-        update_frame()
+            result = f"Predicted Class: {class_labels[predicted_class]}\nConfidence: {confidence:.2f}%"
+            messagebox.showinfo("Prediction Result", result)
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
 
-    def stopCam(self):
-        if self.cam_update_id is not None:
-            self.window.after_cancel(self.cam_update_id)  # after 호출 취소
-            self.cam_update_id = None  # ID 초기화
+    def plotTrainingResults(self, history):
+        """ 학습 결과 시각화 메서드 """
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+        epochs_range = range(len(acc))
+        # 모델의 학습 이력(acc, loss)을 시각화
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
 
-        if self.cap is not None:
-            self.cap.release()
-            self.cap = None
-        
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+
+        plt.tight_layout()
+        plt.show()
+
     def imgRead(self):
+        """ 이미지 불러오기오고 읽는 메서드 """
         file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("img files", "*.jpg"), ("All files", "*.*")])
         img = cv.imread(file_path)
+        # 파일 창을 열어서 탐색
         img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.img_source = img
-        print(img.shape)
-
-        self.window.geometry(str(int(img.shape[1]*2.5)) + "x"+ str(int(img.shape[0])) +"+200+200")
-
-        self.img_source = Image.fromarray(self.img_source)
-        self.img_target = self.img_source.copy()
-        self.img_sourceTK = ImageTk.PhotoImage(image=self.img_source)
-        self.l1.config(image = self.img_sourceTK)
-
-        self.ip = ip.ImageProcessing(np.array(self.img_source))
-
-    def thresholding(self):
-        img = self.ip.sourceImg
-        gray_img = self.ip.toGrayScale(img)
-        thresholded = self.ip.thresholding(gray_img)
-        self.ip.targetImg = thresholded
-        self.img_target = self.ip.targetImg #self.ip.cvtTarget2PIL()
-        
-        messagebox.showinfo("Info", "processing done")
-        
-    def circleDetection(self):
-        img = self.ip.sourceImg
-        gray_img = self.ip.toGrayScale(img)  # 그레이스케일로 변환
-        self.ip.circleDetection(gray_img)    # 원 검출 및 그리기 수행
-        self.img_target = self.ip.sourceImg  # 원이 표시된 이미지를 대상 이미지로 업데이트
-        self.apply()                         # 결과를 GUI에 표시
-        messagebox.showinfo("Info", "원 검출이 완료되었습니다")
 
     def apply(self):
-        self.img_target = Image.fromarray(self.img_target)
-        self.img_targetTK = ImageTk.PhotoImage(image=self.img_target)
-        self.l2.config(image = self.img_targetTK)
+        """ 전처리된 이미지를 GUI에 표시 및 출력하는 메서드 """
+        # 전처리된 이미지를 PIL 및 tkinter 포맷으로 변환 후 레이블에 표시
+        self.img_target = ImageConverter.toPIL(self.img_target)
+        self.img_targetTK = ImageConverter.toPhotoImage(self.img_target)
+        self.l2.config(image=self.img_targetTK)
+
+    def getCam(self):
+        """ 웹캠 영상을 Tkinter GUI에 출력하는 메서드 """
+        cap = cv.VideoCapture(0)
+
+    def stopCam(self):
+        """ 웹캠 중지 메서드 """
+        # 카메라 스트리밍을 중지
+        if self.cam_update_id is not None:
+            self.window.after_cancel(self.cam_update_id)
 
     def render(self):
+        """ GUI 메인 루프 실행 메서드 """
         self.window.mainloop()
 
-tk = MyTkinter()
-tk.initialize()
-
-tk.render()
-
-
-        
+# Main 실행부
+if __name__ == "__main__":
+    tk = MyTkinter()
+    tk.initialize()
+    tk.render()
